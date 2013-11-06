2b52744eefd52c49f22bf289f76903481b7689c1
Caius Durling <dev@caius.name>
2013-11-06 14:20:06 +0000

    Basic buggy implementation

--- src/rprompter/rprompter.go
+++ src/rprompter/rprompter.go
@@ -2,8 +2,34 @@ package main;
 
 import (
   "fmt"
+  "bytes"
+  "log"
+  "time"
+  "os/exec"
 )
 
+func promptTime() string {
+  const layout = "2006-01-02 15:04:05"
+  t := time.Now()
+  // Mon Jan 2 15:04:05 MST 2006
+  return t.Format(layout)
+}
+
+func promptBattery() string {
+  cmd := exec.Command("pmset", "-g", "batt")
+
+  output, err := cmd.Output()
+  if err != nil {
+    log.Fatal(err)
+  }
+
+  output = bytes.Split(output, []byte("\n"))[1]
+  output = bytes.Split(output, []byte("	"))[1]
+  fields := bytes.Split(output, []byte("; "))
+
+  return fmt.Sprintf("%s (%s)\n", fields[0], fields[2])
+}
+
 func main() {
-  fmt.Println("Date here | battery here")
+  fmt.Printf("%s | %s", promptTime(), promptBattery())
 }
